ac0963046ffcfb29286c15feb0c23ddeda25ac26
Gagan Nahak <gagnnahak382@gmail.com>
2026-02-04 17:08:16 +0530

    ccc

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -98,7 +98,7 @@ const{refreshToken,accessToken}=await generateAccessAndRefreshToken(user._id)
 
 const loggedInUser= await User.findById(user._id).select("-password -refreshToken")
 
-return res.status(200).cookie("accessToken",accessToken,{httpOnly:true,secure:true}).cookie("refreshToken",refreshToken,{httpOnly:true,secure:true}).json(
+return res.status(200).cookie("accessToken",accessToken,{httpOnly:true,secure:true,sameSite:none}).cookie("refreshToken",refreshToken,{httpOnly:true,secure:true}).json(
     new ApiResponse(200,{
         user:loggedInUser,accessToken,refreshToken
     },"user logged in..")
